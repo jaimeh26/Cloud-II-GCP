536254c793a5fe8d354426271a6d70c04ea6e672
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-09 19:33:32 -0500

    Update cloud_storage.tf

--- cloud_storage.tf
+++ cloud_storage.tf
@@ -1,1 +1,5 @@
-
+module "cloud-storage_example_multiple_buckets" {
+  source  = "terraform-google-modules/cloud-storage/google//examples/multiple_buckets"
+  version = "5.0.0"
+  # insert the 1 required variable here
+}
