657dfb7b2043aff926f297241f8992566a956718
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-09 19:39:55 -0500

    Delete cloud_storage.tf

--- cloud_storage.tf
+++
@@ -1,5 +0,0 @@
-module "cloud-storage_example_multiple_buckets" {
-  source  = "terraform-google-modules/cloud-storage/google//examples/multiple_buckets"
-  version = "5.0.0"
-  # insert the 1 required variable here
-}
