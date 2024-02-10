c58bf21dea530b61b6bba44f1316567dc6bb295f
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-02-09 19:56:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "project_id" {
   description = "The ID of the Google Cloud project"
+  default = certain-beach-391616
 }
 
 variable "region" {
   description = "The region to create the buckets in"
+  default = us-central1
 }
 
 variable "bucket_names" {
